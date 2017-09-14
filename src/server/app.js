5e4608d5e51c00732b89c5824a344afa750aea77
Nick Meldrum <nick@nickmeldrum.com>
2017-09-14 17:05:15 +0100

    setup ssr route - wip needs root url proxied correctly to server

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,17 @@
+import path from 'path'
 import express from 'express'
+import expressHandlebars from 'express-handlebars'
 
 const app = express()
 
-app.get('/dynamic', (req, res) => res.send('dis dynamic boyyeeee'))
+app.engine('handlebars', expressHandlebars())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, '/views'))
+
+app.get((req, res) => res.send('dis dynamic boyyeeee'))
+
+app.use((req, res) => {
+    res.render('index', { route: req.url })
+})
 
 export default app
