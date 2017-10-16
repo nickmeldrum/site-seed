76be52ea655e63e8138af1d52d1eab07ff6370ee
Nick Meldrum <nick@nickmeldrum.com>
2017-10-16 11:09:26 +0100

    React SSR working

--- src/ssr/app.js
+++ src/ssr/app.js
@@ -1,9 +1,15 @@
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
 import express from 'express'
+
+import Hello from '../ui/components/Hello'
 
 const router = express.Router()
 
 router.use((req, res) => {
-  res.render('index', { route: req.url, data: 'foobar' })
+  res.render('index', { content: ReactDOMServer.renderToString(
+    <Hello />
+  )})
 })
 
 module.exports = router
--- src/ui/index.js
+++ src/ui/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 import Hello from './components/Hello'
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <Hello />,
   window.document.getElementById('container')
 )
